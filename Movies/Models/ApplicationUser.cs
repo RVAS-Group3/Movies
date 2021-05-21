7c60cadad44f4c49006423cf8608c570fe3e5434
Dusica Stevanovic <dstevanovic318s@raf.rs>
2021-05-21 20:53:33 +0200

    Resolved conflicts

---
+++ Movies/Models/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using AspNetCore.Identity.MongoDbCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Movies.Models
+{
+    public class ApplicationUser : MongoIdentityUser
+    {
+        public ApplicationUser() : base()
+        {
+        }
+
+        public ApplicationUser(string userName, string email) : base(userName, email)
+        {
+        }
+    }
+}
---
+++ Movies/Models/ManageViewModels/ConfigureTwoFactorViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Models.ManageViewModels
+{
+    public class ConfigureTwoFactorViewModel
+    {
+        public string SelectedProvider { get; set; }
+
+        public ICollection<SelectListItem> Providers { get; set; }
+    }
+}
