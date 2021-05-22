8c3c95f094653d50add4ff735623f72d4f2abc1e
aleksadjordjevic21s <56649728+aleksadjordjevic21s@users.noreply.github.com>
2021-05-22 14:20:34 +0200

    Prve promene frontend

--- Movies/Controllers/HomeController.cs
+++ Movies/Controllers/HomeController.cs
@@ -24,11 +24,6 @@ namespace Movies.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
